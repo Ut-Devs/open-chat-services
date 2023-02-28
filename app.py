e3451c98d19b886942397e318e2a678ef2765d55
jowjow22 <jonathan224santos@gmail.com>
2023-02-28 15:22:48 -0300

    feat: create base socket connection with dependencies

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def echo(sock):
         sock.send(data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3333))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
