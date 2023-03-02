50e4f7237858b2f52817f94b24649a458924933c
jowjow22 <jonathan224santos@gmail.com>
2023-03-02 09:25:19 -0300

    fix: allow unsafe werkzeug

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins=[])
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
